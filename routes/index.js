f9196376ae3a98bb90c32928045cd3eedba0fedd
Diego Gutiérrez <diegogutcat28@gmail.com>
2022-04-07 00:22:32 -0500

    Ruta para las reservaciones

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,11 @@
 const express = require('express');
 
 const hotelsRouter = require('./hotels.router');
+const reservationsRouter = require('./reservations.router');
 
 function routerApi(app) {
    app.use('/hotels', hotelsRouter);
+   app.use('/reservations', reservationsRouter);
 }
 
 module.exports = routerApi;
