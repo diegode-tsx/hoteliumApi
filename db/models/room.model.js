7268e6a232d06a373aa834a85624882d52958ab5
RomanshkVolkov <chato4010@gmail.com>
2022-04-08 16:48:49 -0500

    Quitamos cantidad de habitaciones del room.model

--- db/models/room.model.js
+++ db/models/room.model.js
@@ -2,10 +2,9 @@ const mongoose = require('mongoose');
 const { Schema } = mongoose;
 
 const roomSchema = new Schema({
-   room_type: {type: String, required: true, maxlength: 50},
-   price: {type: Number, required: true, max: 50000},
-   description : {type: String, required: true, maxlength: 100},
-   amount: {type: String, required: true}//cantidad
+   room_type: { type: String, required: true, maxlength: 50 },
+   price: { type: Number, required: true, max: 50000 },
+   description : { type: String, required: true, maxlength: 100 }
 }, { versionKey: false });
 
 const Room = mongoose.model('Room', roomSchema);
