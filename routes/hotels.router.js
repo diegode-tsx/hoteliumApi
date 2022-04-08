f5f12eb7921322496bac128911647970968a1f82
Diego Gutiérrez <diegogutcat28@gmail.com>
2022-04-08 17:45:04 -0500

    Rutas para la obtención de hoteles por tag y country

--- routes/hotels.router.js
+++ routes/hotels.router.js
@@ -6,14 +6,23 @@ const router = express.Router();
 const service = new HotelService();
 
 router.get('/', async (req, res, next) => {
-   const hotels = await service.find();
+   const detailedRooms = req.query.detailed_rooms;
+   const hotels = await service.find(detailedRooms);
    res.json(hotels);
+});
+
+router.get('/tags/:tag', async (req, res, next) => {
+
+});
+
+router.get('countries/:country', async (req, res, next) => {
+
 });
 
 router.post('/', async (req, res, next) => {
    const body = req.body;
    const newHotel = await service.create(body);
    res.json(newHotel);
-})
+});
 
 module.exports = router;
