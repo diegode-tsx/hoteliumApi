7b5d08a46c69b58dd18f814c04e672fb05ec1195
Diego Gutiérrez <diegogutcat28@gmail.com>
2022-04-09 18:58:35 -0500

    Creación del servicio para obtener los países de la base de datos

--- routes/countries.router.js
+++ routes/countries.router.js
@@ -6,7 +6,12 @@ const router = express.Router();
 const service = new CountryService();
 
 router.get('/', async (req, res, next) => {
-
+   try {
+      const countries = await service.find();
+      res.json(countries);
+   } catch (error) {
+      next(error);
+   }
 });
 
 module.exports = router;
--- services/countries.service.js
+++ services/countries.service.js
@@ -1,10 +1,17 @@
+const boom = require('@hapi/boom');
+const { Country } = require('../db/models/country.model');
+
 class CountriesService {
    constructor() {
 
    }
 
    async find() {
-
+      const countries = await Country.find();
+      if (countries.length === 0) {
+         throw boom.notFound('countries not found');
+      }
+      return countries;
    }
 }
 
