371df9ee63280d35b0a62b29f25d567bc43bb311
Diego Gutiérrez <diegogutcat28@gmail.com>
2022-04-09 17:46:59 -0500

    Cambios menores en el modelo de reservaciones

--- db/models/reservation.model.js
+++ db/models/reservation.model.js
@@ -5,14 +5,14 @@ const { Room } = require('./room.model');
 const reservationSchema = new Schema({
    arrival_date: { type: Date, required: true},
    departure_date: { type: Date, required: true},
-   num_guests: { type: Number, required: true, max: 40 },
-   cant_habs: { type: Number, required: true, max: 30 },
-   total_cost: { type: Number, required: true, min: 1000 },
+   num_guests: { type: Number, required: true, max: 10 },
+   cant_habs: { type: Number, required: true, max: 10 },
+   total_cost: { type: Number, required: true, max: 100000 },
    reservation_code: { type: String, required: true, maxlength: 14 },
    client: {
-      name: { type: String, required: true, maxlength: 80 },
+      name: { type: String, required: true, maxlength: 60 },
       phone: { type: Number, required: true, maxlength: 10},
-      email: { type: String, required: true, maxlength: 70 }
+      email: { type: String, required: true, maxlength: 60 }
    },
    rooms: [{ type: Schema.Types.ObjectId, ref: 'Room' }]
 }, { versionKey: false });
