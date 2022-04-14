18f3adffdaf01ef56f9b78f82123c3a0b18053a7
Diego Gutiérrez <diegogutcat28@gmail.com>
2022-04-14 18:01:49 -0500

    La api ahora captura cualquier posible error de node-mailer

--- libs/nodemailer.js
+++ libs/nodemailer.js
@@ -1,34 +1,25 @@
 const express = require('express');
 const nodemailer = require('nodemailer');
 const transporter = nodemailer.createTransport({
-        service: 'Gmail',
-        auth: {
-            user: 'hoteliumteam@gmail.com',
-            pass: 'roothotelium1'
-        }
-    });
+   service: 'Gmail',
+   auth: {
+      user: 'hoteliumteam@gmail.com',
+      pass: 'roothotelium1'
+   }
+});
 
-function sendMailOp (mailData) {
-    
-    const email = mailData.client.email;
-        var mailOptions = {
-            from: 'Hotelium Reservation Support ✔ <hoteliumteam@gmail.com>',
-            to: email,
-            subject: "Hello " + email,
-            text: 'Hello ' + email + '✔',
-            html: "<p>Hello " + email + " </p>",
-            bcc: email
-        };
-        
-        transporter.sendMail(mailOptions, function(error, info){
-            if(error){
-                console.log(error);
-            }else{
-                console.log('Message sent: ' + info.response);
-                res.send(200);
-            }
-        });        
-    
+async function sendMailOp(mailData) {
+   const email = mailData.client.email;
+   const mailOptions = {
+      from: 'Hotelium Reservation Support ✔ <hoteliumteam@gmail.com>',
+      to: email,
+      subject: "Hello " + email,
+      text: 'Hello ' + email + '✔',
+      html: "<p>Hello " + email + " </p>",
+      bcc: email
+   };
+
+   return await transporter.sendMail(mailOptions);
 }
 
 module.exports = sendMailOp;
