fef7df7471d89c22b073de8be288aa39b935bfe1
root <root@Romashk-Volkov.localdomain>
2022-04-14 16:10:48 -0500

    Envio de correo funcional al registrarse

--- routes/index.js
+++ routes/index.js
@@ -3,13 +3,11 @@ const express = require('express');
 const hotelsRouter = require('./hotels.router');
 const reservationsRouter = require('./reservations.router');
 const countriesRouter = require('./countries.router');
-const emailRouter = require('./nodemailer.route');
 
 function routerApi(app) {
    app.use('/hotels', hotelsRouter);
    app.use('/reservations', reservationsRouter);
    app.use('/countries', countriesRouter);
-   app.use('/email', emailRouter);
 }
 
 module.exports = routerApi;
