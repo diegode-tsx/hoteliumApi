59cf12d6179af84c3e90d5979c7f9137312bb746
root <root@Romashk-Volkov.localdomain>
2022-04-14 21:40:00 -0500

    Asignar credenciales nodemailer por archivo .env la wena

--- libs/nodemailer.js
+++ libs/nodemailer.js
@@ -7,21 +7,12 @@ const USER = encodeURI(config.emailUser);
 const PASS = encodeURI(config.emailPass);
 
 const transporter = nodemailer.createTransport({
-<<<<<<< HEAD
         service: 'Gmail',
         auth: {
             user: USER,
             pass: PASS
         }
     });
-=======
-   service: 'Gmail',
-   auth: {
-      user: 'hoteliumteam@gmail.com',
-      pass: 'roothotelium1'
-   }
-});
->>>>>>> eddb1b403de87927faf67fb29bd213decc98c397
 
 async function sendMailOp(mailData) {
    const email = mailData.client.email;
